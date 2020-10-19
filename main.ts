0e68d03dadf261485b0c761e578cad8070dd0cde
yuanaiqing <yuan_aiqing@yesdata.net>
2020-10-19 22:31:35 +0800

    add leg_raise

--- main.ts
+++ main.ts
@@ -4,3 +4,19 @@ function 抬腿 () {
 basic.forever(function () {
 	
 })
+
+// Icon unicode characters can be found at: http://fontawesome.io/icons/
+//% color=#0000ff weight=100 icon="robot" block="SimpleRobot" advanced=false
+namespace simpleRobot {
+	/**
+     * How many bananas?
+     * @param n number of bananas
+     */
+    // Info about defining blocks: https://makecode.com/defining-blocks
+    //% blockId=leg_raise
+    //% block="抬腿"
+    export function leg_raise() {
+        basic.showNumber(9);
+    }
+
+}
